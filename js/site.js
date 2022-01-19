5b2da98b66b3d1a6e2603606732f560ae1011756
Bmart1108 <bmart1108@gmail.com>
2022-01-19 12:57:48 -0500

    Added Javascript

--- js/site.js
+++ js/site.js
@@ -1,14 +1,32 @@
-// display a message to the user
-function getMessage() {
 
-    let userMessage = document.getElementById("message").value;
 
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Your work has been saved',
-        showConfirmButton: false,
-        timer: 1500
-    })
-    console.log(userMessage);
-};
+// pull the user entered string from the page
+function getValues() {
+    let userString = document.getElementById("revString").value;
+
+    //call reverse a string
+    let revString = reverseAString(userString);
+
+    //Display the reverse string   
+    displayData(revString);
+}
+
+// this is out generate data function
+function reverseAString(userString) {
+
+    let revString = "";
+
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+
+    }
+
+    return revString;
+}
+
+//display out reversed string to the page
+function displayData(revString) {
+
+    document.getElementById("msg").innerHTML = `Your String Reversed is: ${revString}`;
+    document.getElementById("alert").classList.remove("invisible");
+}
